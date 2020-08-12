aa5705fdea73708007b188b902285033320118b0
Evgeny Artemov <eartemov013@gmail.com>
2020-08-12 11:42:17 +0300

    concatenation fix

--- src/Components/SideMenuItems.jsx
+++ src/Components/SideMenuItems.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './SideMenu.scss';
 
 export default function SideMenuItems({ menuItems }) {
@@ -11,14 +11,14 @@ export default function SideMenuItems({ menuItems }) {
     setClosed(!closed);
   }
   return (
-    <div className={'sidemenu__block' + `${checkClosed}`} onClick={openHandler}>
-      <div className={'sidemenu__title home' + `${checkClosed} ${menuItems.header.iconClass}`}>
+    <div className={`sidemenu__block ${checkClosed}`} onClick={openHandler}>
+      <div className={`sidemenu__title home ${checkClosed} ${menuItems.header.iconClass}`}>
         <NavLink to={`/${menuItems.header.iconClass}`}>{menuItems.header.text}</NavLink>
       </div>
-      <ul className={'sidemenu__list' + `${checkClosed}`}>
+      <ul className={`sidemenu__list ${checkClosed}`}>
         {menuItems?.items.map((el) => {
           return (
-            <li key={`${el.iconClass}`} className={'sidemenu__list_item ' + `${el.iconClass}`}>
+            <li key={`${el.iconClass}`} className={`sidemenu__list_item  ${el.iconClass}`}>
               <NavLink to={`/${el.iconClass}`}>{el.text}</NavLink>
             </li>
           );
